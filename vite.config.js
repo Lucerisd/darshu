6d494451e04e991713e28831c6d44dadd0388a4d
Lucerisd <lucerisd@users.noreply.github.com>
2026-09-13 12:13:37 +0000

    Update folder name from zainabkabira.com to darshitagupta.in in gitignore and vite config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     open: false,
     host: true,
     watch: {
-      ignored: ['**/dist/**', '**/.git/**', '**/.vercel/**', '**/.netlify/**', '**/assets/**', '**/assets2/**', '**/node_modules/**']
+      ignored: ['**/dist/**', '**/.git/**', '**/.vercel/**', '**/.netlify/**', '**/assets/**', '**/assets2/**', '**/node_modules/**', '**/darshitagupta.in/**', '**/zainabkabira.com/**']
     }
   },
   preview: {
